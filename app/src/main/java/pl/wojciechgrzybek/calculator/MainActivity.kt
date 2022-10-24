a8158f084a290665c90d4c70f70be65af1eeed7e
grzybi <grzybi4spam@gmail.com>
2022-10-24 10:19:05 +0200

    Added listeners fo buttons in Main Activity.

--- app/src/main/java/pl/wojciechgrzybek/calculator/MainActivity.kt
+++ app/src/main/java/pl/wojciechgrzybek/calculator/MainActivity.kt
@@ -1,11 +1,35 @@
 package pl.wojciechgrzybek.calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSimple: Button = findViewById(R.id.btnSimple)
+        btnSimple.setOnClickListener {
+            Toast.makeText(this, "to be implemented", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnAdvanced: Button = findViewById(R.id.btnAdvanced)
+        btnAdvanced.setOnClickListener {
+            Toast.makeText(this, "to be implemented", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnAbout: Button = findViewById(R.id.btnAbout)
+        btnAbout.setOnClickListener {
+            Toast.makeText(this, "to be implemented", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnExit: Button = findViewById(R.id.btnExit)
+        btnExit.setOnClickListener {
+            Toast.makeText(this, "to be implemented", Toast.LENGTH_SHORT).show()
+        }
     }
 }

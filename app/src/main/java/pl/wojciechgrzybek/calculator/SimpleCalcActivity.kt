c6e767b1b61c379939eb6c119cd56bc7dd367923
grzybi <grzybi4spam@gmail.com>
2022-10-24 13:08:12 +0200

    Added listeners for all buttons.

--- app/src/main/java/pl/wojciechgrzybek/calculator/SimpleCalcActivity.kt
+++ app/src/main/java/pl/wojciechgrzybek/calculator/SimpleCalcActivity.kt
@@ -29,10 +29,45 @@ class SimpleCalcActivity : AppCompatActivity() {
         (findViewById<Button>(R.id.button7)).setOnClickListener {onNumberButtonClicked(R.string.btn7)}
         (findViewById<Button>(R.id.button8)).setOnClickListener {onNumberButtonClicked(R.string.btn8)}
         (findViewById<Button>(R.id.button9)).setOnClickListener {onNumberButtonClicked(R.string.btn9)}
+
+        (findViewById<Button>(R.id.buttonAdd)).setOnClickListener {onOperatorButtonClicked(R.string.btnAdd)}
+        (findViewById<Button>(R.id.buttonSub)).setOnClickListener {onOperatorButtonClicked(R.string.btnSub)}
+        (findViewById<Button>(R.id.buttonMul)).setOnClickListener {onOperatorButtonClicked(R.string.btnMul)}
+        (findViewById<Button>(R.id.buttonDiv)).setOnClickListener {onOperatorButtonClicked(R.string.btnDiv)}
+
+        (findViewById<Button>(R.id.buttonSign)).setOnClickListener {onSignButtonClicked()}
+        (findViewById<Button>(R.id.buttonDot)).setOnClickListener {onDotButtonClicked()}
+        (findViewById<Button>(R.id.buttonClear)).setOnClickListener {onClearButtonClicked()}
+        (findViewById<Button>(R.id.buttonResult)).setOnClickListener {onEqualButtonClicked()}
     }
 
     private fun onNumberButtonClicked(label: Int) {
         Log.d("Number Button", getString(label))
         display.text = getString(label)
+    }
+
+    private fun onOperatorButtonClicked(label: Int) {
+        Log.d("Operator Button", getString(label))
+        display.text = getString(label)
+    }
+
+    private fun onDotButtonClicked() {
+        Log.d("Dot Button", getString(R.string.btnDot))
+        display.text = getString(R.string.btnDot)
+    }
+
+    private fun onSignButtonClicked() {
+        Log.d("Sign Button", getString(R.string.btnSign))
+        display.text = getString(R.string.btnSign)
+    }
+
+    private fun onClearButtonClicked() {
+        Log.d("Clear Button", getString(R.string.btnClear))
+        display.text = "0"
+    }
+
+    private fun onEqualButtonClicked() {
+        Log.d("Equal Button", getString(R.string.btnResult))
+        display.text = getString(R.string.btnResult)
     }
 }
